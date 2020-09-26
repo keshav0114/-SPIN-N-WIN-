283515504c0fcb79278df4f8e8735a156316e8e9
Keshav <keshavarora0114@gmail.com>
2020-09-26 17:18:33 +0530

    Final

---
+++ index.js
@@ -0,0 +1,56 @@
+function myfunction(){
+    let spinsound = new Audio();
+    spinsound.src = "spin-sound.mp3";
+
+    spinsound.play();
+   
+    setTimeout(function(){ document.getElementById('move').click(); }, 6000);   
+
+    let x=1024;
+    let y=9999;
+
+    let deg= Math.floor(Math.random()*(x - y)+y);
+
+    console.log(deg);
+    
+    document.getElementById('box').style.transform ="rotate("+deg+"deg)";
+}
+function getinputvalue(){
+    let x=document.getElementById("input1").value;
+   if(x==2)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+   
+    let winsound = new Audio();
+    winsound.src = "win-sound.mp3";
+
+    winsound.play();
+   }
+   else if(x==5)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+    let winsound = new Audio();
+    winsound.src = "win-sound.mp3";
+
+    winsound.play();
+   }
+   else if(x==7)
+   {
+    document.getElementById("lose").style.display="none";
+    document.getElementById("winner").style.display="block";
+    let winsound = new Audio();
+    winsound.src = "win-sound.mp3";
+
+    winsound.play();
+   }
+   else{
+    document.getElementById("winner").style.display="none";   
+    document.getElementById("lose").style.display="block";
+    let losesound = new Audio();
+    losesound.src = "lose-sound.mp3";
+
+    losesound.play();
+  }
+}
